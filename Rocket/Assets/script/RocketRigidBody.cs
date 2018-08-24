0c3f586cc45fb3c05b3ef885c4ff868c5f8a2150
B533P-08\533 <pluc113@hotmail.com>
2018-08-24 16:36:22 -0400

    Ajout de plein de forme et script et material

---
+++ Rocket/Assets/script/RocketRigidBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketRigidBody : MonoBehaviour {
+    public Rigidbody rocketRigidBody;
+    [SerializeField] public float ForceUpward = 2000;
+    // Use this for initialization
+    void Start () {
+        rocketRigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rocketRigidBody.AddForce(new Vector3(0, ForceUpward, 0));
+        }
+    }
+}
