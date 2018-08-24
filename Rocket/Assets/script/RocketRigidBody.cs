1e613b61965e09953ccba1e595f231a164382c55
B533P-08\533 <pluc113@hotmail.com>
2018-08-24 16:52:36 -0400

    Modification du code pour que la fusée puisse tourner.

--- Rocket/Assets/script/RocketRigidBody.cs
+++ Rocket/Assets/script/RocketRigidBody.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RocketRigidBody : MonoBehaviour {
     public Rigidbody rocketRigidBody;
     [SerializeField] public float ForceUpward = 2000;
+    [SerializeField] public float Trust = 20;
+    [SerializeField] public float pointForce = 2;
     // Use this for initialization
     void Start () {
         rocketRigidBody = GetComponent<Rigidbody>();
@@ -12,9 +14,27 @@ public class RocketRigidBody : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        ManageInputVertivcal();
+        ManageInputHorizontal();
+    }
+
+    private void ManageInputVertivcal()
+    {
         if (Input.GetAxis("Jump") > 0)
         {
             rocketRigidBody.AddForce(new Vector3(0, ForceUpward, 0));
+        }
+    }
+
+    private void ManageInputHorizontal()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rocketRigidBody.AddForceAtPosition(new Vector3(0, Trust, 0),(new Vector3(pointForce, 0, 0)));
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rocketRigidBody.AddForceAtPosition(new Vector3(0, -Trust, 0),(new Vector3(pointForce, 0, 0)));
         }
     }
 }
